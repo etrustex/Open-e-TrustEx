9cfaf90a10c111ec00a01f61bb45d11ec773a28f
almahfa <fassifehri.m@gmail.com>
2019-10-15 12:17:46 +0200

    Removing unneeded folders

--- 003 Architecture/001 Data Model/200 XSD/schematron/schematron_generation/utility/w3cschema.sh
+++
@@ -1,3 +0,0 @@
-#echo Validating $2 with $1...
-SCHEMAPATH=`echo $0 | sed s,[^/]*$,, | sed s,^$,./,`
-java -cp "${SCHEMAPATH}xjparse.jar:${SCHEMAPATH}resolver.jar:${SCHEMAPATH}xercesImpl.jar:${SCHEMAPATH}" com.nwalsh.parsers.xjparse -c "${SCHEMAPATH}catalog.xml" -S $1 $2
